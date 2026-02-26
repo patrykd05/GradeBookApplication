eca4346637f2a126f5378ffa69ef78fc32a84671
patrykd2115 <dendorpatryk@gmail.com>
2026-02-26 16:37:09 +0100

    Update StandardGradeBook Type

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    internal class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+         
+        }
+    }
+}
