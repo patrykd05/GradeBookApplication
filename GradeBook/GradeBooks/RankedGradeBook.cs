c55200add3bb10fc1f76cf84b360b4dc896bff8e
patrykd2115 <dendorpatryk@gmail.com>
2026-02-26 17:45:08 +0100

    OverrideRankedGradeBooksGetLetterGradeTests

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -2,14 +2,49 @@
 using System.Collections.Generic;
 using System.Text;
 using GradeBook.Enums;
+using Microsoft.VisualBasic;
 
 namespace GradeBook.GradeBooks
 {
     internal class RankedGradeBook : BaseGradeBook
     {
+
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+
         }
-    }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            int groupSize = (int)Math.Ceiling(Students.Count * 0.2);
+
+            int higher = 0;
+
+            foreach (var student in Students)
+            {
+                if (student.AverageGrade > averageGrade)
+                    higher++;
+            }
+
+            if (higher < groupSize)
+                return 'A';
+            else if (higher < groupSize * 2)
+                return 'B';
+            else if (higher < groupSize * 3)
+                return 'C';
+            else if (higher < groupSize * 4)
+                return 'D';
+            else
+                return 'F';
+
+
+        }
+    }      
 }
