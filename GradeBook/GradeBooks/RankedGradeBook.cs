250bafc4793f774aa2e6129fc4448202ea4bb293
patrykd2115 <dendorpatryk@gmail.com>
2026-02-26 17:51:08 +0100

    CreateStatisticsOverridesTests

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -43,8 +43,30 @@ namespace GradeBook.GradeBooks
                 return 'D';
             else
                 return 'F';
-
-
         }
-    }      
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else if (Students.Count >= 5)
+            {
+                base.CalculateStatistics();
+            }
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else if (Students.Count >= 5)
+            {
+                base.CalculateStudentStatistics(name);
+            }
+        }
+    }
 }
